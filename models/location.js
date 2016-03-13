937192330cc7d0816a087267003651732385b987
quinnvh <qhaastre@student.avans.nl>
2016-03-13 12:08:20 +0100

    New start

--- models/location.js
+++
@@ -1,9 +0,0 @@
-function init(mongoose){
-	console.log('Initializing location schema');
-	var schema = mongoose.Schema({
-		place_id: { type: Number, unique: true, required: true }
-	});
-	mongoose.model('Location', schema);
-}
-
-module.exports = init;
